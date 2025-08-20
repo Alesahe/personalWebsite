047dce96f5310af91ebdeab90a5127e4eef92ed1
alesahe <confuzzlementation@gmail.com>
2025-08-20 19:33:47 +1000

    added other tabs

--- server.js
+++ server.js
@@ -9,5 +9,14 @@ app.get("/", function (req, res) {
 app.get("/books", function (req, res) {
   res.sendFile(path.join(__dirname, "public/html/books.html"));
 });
+app.get("/adventures", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/html/adventures.html"));
+});
+app.get("/snails", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/html/snails.html"));
+});
+// app.get("/shells", function (req, res) {
+//   res.sendFile(path.join(__dirname, "public/html/shells.html"));
+// });
 
 app.listen(8000, () => console.log("port 8000: http://localhost:8000/ or http://127.0.0.1:8000") );
